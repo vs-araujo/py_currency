b737fde0f0f81ee6b3c71fe1f9ee13b66e9f3c84
Vítor Araujo <v.s.araujo@protonmail.com>
2022-04-06 10:53:21 -0300

    Changed 2nd argument of init to 'format'

--- py_currency.py
+++ py_currency.py
@@ -1,29 +1,33 @@
 class Currency:
-    symbol = ""
+    symbol = ("", "")
     decimal = "."
     milliard = ""
 
-    def __init__(self, value, cents=False):
-        if cents:
+    def __init__(self, value, format=None):
+        if format == "cents":
             if isinstance(value, int):
                 self.value = value
             else:
-                err_msg = "When 'cents=True', 'value' must be an integer'"
-                raise ValueError(err_msg)
+                err_msg = "When format='cents', 'value' must be an integer"
+                raise ValueError(err_msg) from None
 
-        else:
+        elif format is None:
             try:
                 self.value = int(f"{value:.2f}".replace(".", ""))
             except ValueError:
                 err_msg = f"Cannot convert {type(value)} to {self.__class__}"
                 raise ValueError(err_msg) from None
 
+        else:
+            raise ValueError(
+                f"'{format}' is not a valid argument for 'format'")
+
     def __str__(self):
         value = self.value / 100
         value = f"{value:,.2f}".replace(",", "_")
         value = value.replace(".", self.decimal)
         value = value.replace("_", self.milliard)
-        return f"{self.symbol} {value}"
+        return f"{self.symbol[0]} {value} {self.symbol[1]}".strip()
 
     def __repr__(self):
         return self.__str__()
@@ -37,7 +41,7 @@ class Currency:
             except (ValueError, TypeError):
                 err_msg = f"Cannot add {type(other)} and {type(self)}"
                 raise ValueError(err_msg) from None
-        return self.__class__(round(value / 100, 2))
+        return self.__class__(round(value), format="cents")
 
     def __sub__(self, other):
         if isinstance(other, self.__class__):
@@ -48,13 +52,13 @@ class Currency:
             except (ValueError, TypeError):
                 err_msg = f"Cannot subtract {type(other)} and {type(self)}"
                 raise ValueError(err_msg) from None
-        return self.__class__(round(value / 100, 2))
+        return self.__class__(round(value), format="cents")
 
     def __mul__(self, other):
-        return self.__class__(round(self.value * other / 100, 2))
+        return self.__class__(round(self.value * other), format="cents")
 
     def __truediv__(self, other):
-        return self.__class__(round(self.value / (100 * other), 2))
+        return self.__class__(round(self.value / other), format="cents")
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -113,12 +117,12 @@ class Currency:
 
 
 class BRL(Currency):
-    symbol = "R$"
+    symbol = ("R$", "")
     decimal = ","
     milliard = "."
 
 
 class USD(Currency):
-    symbol = "US$"
+    symbol = ("US$", "")
     decimal = "."
     milliard = ","
